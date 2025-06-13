a36586cf143c4440126bc7a2f7ea8aed05314f32
xingyu <1294266616@qq.com>
2025-06-14 00:56:17 +0800

    ✨ feat: 更新狼人杀插件的更新指令格式
    修改更新指令的正则表达式，使其支持更灵活的输入格式。同时，更新回复消息内容，提升用户体验。

--- apps/update.js
+++ apps/update.js
@@ -9,7 +9,7 @@ export class WerewolfUpdate extends plugin {
       priority: 1000,
       rule: [
         {
-          reg: "^#*狼人杀(插件)?(强制)?更新$",
+          reg: "^#*(强制)?更新狼人杀(插件)?$",
           fnc: "update"
         },
         {
@@ -22,7 +22,7 @@ export class WerewolfUpdate extends plugin {
 
   async update(e = this.e) {
     if (!e.isMaster) return e.reply("你没有权限更新狼人杀插件")
-      e.msg = `#${e.msg.includes("强制") ? "强制" : ""}更新werewolf-plugin`
+      e.msg = `#${e.msg.includes("强制") ? "强制" : ""}更新狼人杀`
       const up = new Update(e)
       up.e = e
       return up.update()
